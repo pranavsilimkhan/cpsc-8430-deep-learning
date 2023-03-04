8ca13df4f0e4fefb5ba5ef22a3cbb112707ca2bf
Pranav <pranav.silimkhan@gmail.com>
2023-03-04 16:32:23 -0500

    Added files realted to hw2

---
+++ hw2/hw2_1/test.py
@@ -0,0 +1,44 @@
+import sys
+import torch
+import json
+from compute import test_data, test, MODELS, encoderRNN, decoderRNN, attention
+from torch.utils.data import DataLoader
+from bleu_eval import BLEU
+import pickle
+
+model = torch.load('./SavedModel/model_1/model0.h5', map_location=lambda storage, loc: storage)
+filepath = './data/testing_data/feat'
+
+dataset = test_data('{}'.format(sys.argv[1]))
+testing_loader = DataLoader(dataset, batch_size=32, shuffle=True)
+
+with open('i2w.pickle', 'rb') as handle:
+    i2w = pickle.load(handle)
+
+# model = model.cuda()
+ss = test(testing_loader, model, i2w)
+
+with open(sys.argv[2], 'w') as f:
+    for id, s in ss:
+        f.write('{},{}\n'.format(id, s))
+
+
+test = json.load(open('./data/testing_label.json'))
+output = sys.argv[2]
+result = {}
+with open(output,'r') as f:
+    for line in f:
+        line = line.rstrip()
+        comma = line.index(',')
+        test_id = line[:comma]
+        caption = line[comma+1:]
+        result[test_id] = caption
+
+bleu=[]
+for item in test:
+    score_per_video = []
+    captions = [x.rstrip('.') for x in item['caption']]
+    score_per_video.append(BLEU(result[item['id']],captions,True))
+    bleu.append(score_per_video[0])
+average = sum(bleu) / len(bleu)
+print("BLEU Score " + str(average))
